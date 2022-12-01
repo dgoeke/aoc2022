8f41a29e61c0ee34483df909b6f84f01e801c198
David Goeke <dg@github.dgoeke.io>
2022-11-30 21:13:08 -0800

    day 1

--- src/aoc2022/core.clj
+++ src/aoc2022/core.clj
@@ -10,3 +10,11 @@
            io/resource
            slurp
            str/trim))))
+
+(def sample
+  (memoize
+    (fn [^Integer n]
+      (->> (format "day%d-sample.txt" n)
+           io/resource
+           slurp
+           str/trim))))
--- src/aoc2022/day1.clj
+++ src/aoc2022/day1.clj
@@ -1,5 +1,21 @@
 (ns aoc2022.day1
-  (:require [aoc2022.core :refer [day-lines]]))
+  (:require [aoc2022.core :as aoc]
+            [clojure.string :as str]))
 
-(def input
-  (map #(Integer/parseInt %) (day-lines 1)))
+(defn calories-per-elf [data]
+  (->> (str/split data #"\n\n")
+       (map str/split-lines)
+       (map (partial map #(Integer/parseInt %)))
+       (map (partial reduce +))))
+
+(defn part1 [data]
+  (apply max (calories-per-elf data)))
+
+(defn part2 [data]
+  (->> (calories-per-elf data)
+       (sort #(compare %2 %1))
+       (take 3)
+       (reduce +)))
+
+;; (part1 (aoc/day 1)) ;=> 69289
+;; (part2 (aoc/day 1)) ;=> 205615
